d850a71a153156409a5b938913cb68bacc1df88f
Xiao Wang <xiaow.wang@mail.utoronto.ca>
2021-03-10 16:19:18 -0500

    my commit

---
+++ config/index.js
@@ -0,0 +1,107 @@
+module.exports = {
+  //-- SITE SETTINGS -----
+  author: "XiaoWang",
+  siteTitle: "Personal Website",
+  siteShortTitle: "X.W.", // Used as logo text in header, footer, and splash screen
+  siteDescription: "",
+  siteUrl: "https://imxiaow.github.io",
+  siteLanguage: "en_US",
+  siteIcon: "content/favicon.png", // Relative to gatsby-config file
+  seoTitleSuffix: "Imprint - {Xiao Wang Porfolio}", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
+  useCookieBar: false, // If you use Google Analytics and want to be GDPR-compliant, set it to true
+  googleAnalyticsTrackingId: "", // e.g. UA-XXXXXX-X
+
+  // -- THEME SETTINGS -----
+  colors: {
+    lightTheme: {
+      primary: "#000000",
+      secondary: "#FFF4D9",
+      tertiary: "#F2F2F2",
+      text: "#000000",
+      subtext: "#555555",
+      background: "#FFFFFF",
+      card: "#FFFFFF",
+      scrollBar: "rgba(0, 0, 0, 0.5)",
+      boxShadow: "rgba(0, 0, 0, 0.16)",
+      boxShadowHover: "rgba(0, 0, 0, 0.32)",
+    },
+    darkTheme: {
+      primary: "#FAFAFA",
+      secondary: "#2A2926",
+      tertiary: "#252525",
+      text: "rgba(255, 255, 255, 0.87)",
+      subtext: "#AAAAAA",
+      background: "#121212",
+      card: "#1C1C1C",
+      scrollBar: "rgba(255, 255, 255, 0.5)",
+      boxShadow: "rgba(0, 0, 0, 0.16)",
+      boxShadowHover: "rgba(0, 0, 0, 0.32)",
+    },
+  },
+  fonts: {
+    primary: "Roboto, Arial, sans-serif",
+  },
+
+  //-- ARTICLES SECTION SETTINGS -----
+  // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
+  // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
+  mediumRssFeed: "",
+  // rssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.theguardian.com%2Finternational%2Frss",
+
+  shownArticles: 3,
+
+  //-- SOCIAL MEDIA SETTINGS -----
+  // There are icons available for the following platforms:
+  // Medium, GitHub, LinkedIn, XING, Behance, E-Mail
+  socialMedia: [
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/imxiaow/",
+    },
+    {
+      name: "Twitter",
+      url: "https://twitter.com/xiaow9596",
+    },
+    {
+      name: "Github",
+      url: "https://github.com/imxiaow",
+    },
+    {
+      name: "Email",
+      url: "mailto:xiaow.wang@mail.utoronto.ca",
+    },
+  ],
+
+  //-- NAVIGATION SETTINGS -----
+  navLinks: {
+    menu: [
+      {
+        name: "About Me",
+        url: "/#about",
+      },
+      {
+        name: "Projects",
+        url: "/#projects",
+      },
+      {
+        name: "Contact",
+        url: "/#contact",
+      },
+    ],
+    button: {
+      useFileName: true,
+      name: "CV",
+      fileName: "XWANG_CV_2021_FEB_Full.pdf", // the file has to be placed inside the static folder at the root level
+      url: "", // if useFileName=false, you can set an anchor link here and use the button for navigational purposes
+    },
+  },
+  footerLinks: [
+    {
+      name: "Privacy",
+      url: "/privacy",
+    },
+    {
+      name: "All Rights Reserved",
+    },
+  ],
+}
